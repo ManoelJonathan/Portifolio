ce38aeb95842513a689838cac368a8717ae7cc8d
Manoel <manoel_jonathan@hotmail.com>
2025-12-17 13:05:20 -0300

    feat: adding struture to the weunite project page

--- src/components/projets/WeuniteProjets.tsx
+++ src/components/projets/WeuniteProjets.tsx
@@ -1,12 +1,12 @@
-
 import { ArrowLeft } from "lucide-react";
 import { Link } from "react-router-dom";
 import { Button } from "../ui/shadcn/button";
+import { CardBody, CardContainer, CardItem } from "../ui/3d-card";
 
 export default function WeuniteProjets() {
   return (
     <div className="min-h-screen pt-[12vh] px-4 flex flex-col items-center text-primary">
-      <div className="w-full max-w-4xl mb-8">
+      <div className="w-full justify-start items-start">
         <Link to="/">
           <Button variant="outline" className="gap-2">
             <ArrowLeft className="h-4 w-4" />
@@ -14,10 +14,54 @@ export default function WeuniteProjets() {
           </Button>
         </Link>
       </div>
-      
-      <div className="text-2xl font-bold">
-        Olá
-      </div>
+
+    <div>
+        <h2>Projeto WeUnite</h2>
+    </div>
+      <CardContainer className="inter-var">
+        <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[45rem] h-auto rounded-xl p-6 border  ">
+          <CardItem
+            translateZ="50"
+            className="text-xl font-bold text-neutral-600 dark:text-white"
+          >
+            Make things float in air
+          </CardItem>
+          <CardItem
+            as="p"
+            translateZ="60"
+            className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+          >
+            Hover over this card to unleash the power of CSS perspective
+          </CardItem>
+          <CardItem translateZ="100" className="w-full mt-4">
+            <img
+              src="./Horizontal.png"
+              height="1000"
+              width="1000"
+              className="h-auto w-auto object-cover rounded-xl group-hover/card:shadow-xl"
+              alt="thumbnail"
+            />
+          </CardItem>
+          <div className="flex justify-between items-center mt-10">
+            <CardItem
+              translateZ={20}
+              as="a"
+              href="https://twitter.com/mannupaaji"
+              target="__blank"
+              className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+            >
+              Try now →
+            </CardItem>
+            <CardItem
+              translateZ={20}
+              as="button"
+              className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+            >
+              Sign up
+            </CardItem>
+          </div>
+        </CardBody>
+      </CardContainer>
     </div>
   );
 }
