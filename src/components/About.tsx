1f8202ce080feded1d117a9e345b927eb6ef7a76
Manoel <manoel_jonathan@hotmail.com>
2026-01-21 21:23:50 -0300

    feat(about): adding my resume download

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,4 +1,4 @@
-import { ArrowDownIcon } from "lucide-react";
+import { ArrowDownIcon, Download } from "lucide-react";
 import { Link } from "react-router-dom";
 import { CardSpotlight } from "@/components/ui/card-spotlight";
 import { skillsData } from "@/data/AboutData";
@@ -21,12 +21,30 @@ export default function About() {
       </div>
 
       <div className="flex flex-col xl:flex-row max-w-7xl text-center xl:text-left px-4 gap-12 items-center">
-        <p className="text-lg text-muted-foreground font-space leading-relaxed flex-1 max-w-2xl">
-          Olá! Sou um desenvolvedor foco em desenvolvimento full stack, algumas das linguagens que mais trabalho
-          são JavaScript e o TypeScript, além da utlização da biblioteca React para o frontend. No backend, tenho experiência
-          com Java com Spring Boot, com arquitetura MVC e padrões RESTful. Meus conhecimentos também abragem bancos de dados relacionais
-          como MySQL e PostgresSQL, além disto venho desenvolvendo noções em bancos de dados não relacionais como MongoDB.
-        </p>
+        <div className="flex flex-col gap-10 items-center">
+          <p className="text-lg text-muted-foreground font-space leading-relaxed flex-1 max-w-2xl text-justify">
+            Olá! Sou um desenvolvedor foco em desenvolvimento full stack,
+            algumas das linguagens que mais trabalho são JavaScript e o
+            TypeScript, além da utlização da biblioteca React para o frontend.
+            No backend, tenho experiência com Java com Spring Boot, com
+            arquitetura MVC e padrões RESTful. Meus conhecimentos também abragem
+            bancos de dados relacionais como MySQL e PostgresSQL, além disto
+            venho desenvolvendo noções em bancos de dados não relacionais como
+            MongoDB.
+          </p>
+
+          <a
+            href="/Currículo-Manoel_Jonathan.pdf"
+            download="Manoel_Currículo.pdf"
+            >
+            <button className="relative inline-flex h-10 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+              <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+              <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-20 py-1 text-sm font-medium text-white backdrop-blur-3xl gap-5">
+                <Download /> Baixar Currículo
+              </span>
+            </button>
+          </a>
+        </div>
 
         <div className="w-full flex-1 grid grid-cols-1 md:grid-cols-2 gap-4 ">
           {skillsData.map((group) => (
@@ -40,7 +58,10 @@ export default function About() {
 
               <div className="grid grid-cols-3 gap-3">
                 {group.content.map((skill) => (
-                  <div key={skill.name} className="flex flex-col items-center gap-1">
+                  <div
+                    key={skill.name}
+                    className="flex flex-col items-center gap-1"
+                  >
                     {typeof skill.icon === "string" ? (
                       <img
                         src={skill.icon}
